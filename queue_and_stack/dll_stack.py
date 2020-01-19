060a939bd22031ae369146fa19c542ecab27ed79
Miguel Peralta <eudysperalta723@gmail.com>
2020-01-19 11:49:31 -0800

    second go at queue and stacks...great practive

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -7,12 +7,26 @@ class Stack:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size+=1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.size-= 1
+            self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
+
+    '''
+        how to traverse a linked list without a tail
+        current_node = self.head
+        while curret_node.next is not None:
+            if current_node.next is None:
+                self.tail is current_node
+            current_node = current_node.next
+
+    '''
